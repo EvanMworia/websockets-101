bd23aaf920d298c687d3a3249f82c16753254549
EvanMworia <evannji99@gmail.com>
2025-04-19 00:01:17 +0300

    Server update

--- ws/Backend/server.js
+++ ws/Backend/server.js
@@ -8,12 +8,12 @@ wss.on('connection', (ws) => {
 	console.log('New client connected!');
 
 	ws.send('👋 Welcome to the WebSocket server!');
-// event driven listenning for message
+	// event driven listenning for message
 	ws.on('message', (message) => {
 		console.log('Received:', message);
 		ws.send(`You said: ${message}`);
 	});
-
+	//event driven to listen for disconnection from client
 	ws.on('close', () => {
 		console.log('Client disconnected');
 	});
